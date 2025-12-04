0dff211d19203829e046a0b8132650a2532245f2
7OCbefore <7OCbefore@gmail.com>
2025-12-04 15:26:36 +0800

    同上一次

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // 替换 Supabase 项目信息
 const supabaseUrl = 'https://wpnowjoxpdnqfvcbhavg.supabase.co'
-const supabaseKey = 'sb_publishable_PLQGSvVQN5Wiq1Rjex59GA_Nl5QlZ3Q'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indwbm93am94cGRucWZ2Y2JoYXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3Njc0MjksImV4cCI6MjA4MDM0MzQyOX0.LsicG-VZEYTgtbVvO5aCNonMrWQO_IDn14f8DYVWbzw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
